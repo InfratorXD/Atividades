b385aa5e7e42019b7186a29d2a4c563dce2d6a42
InfratorXD <guiguicazeee@gmail.com>
2023-08-29 11:19:10 -0300

    atividades

---
+++ Produto simples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    int main(){
+    int valor1;
+    int valor2;
+    int PROD;
+
+    scanf("%d",&valor1);
+    scanf("%d",&valor2);
+    
+    PROD = valor1*valor2;
+
+    printf("PROD = %d\n",PROD);
+    
+        return 0; //professor, não estou fazendo de uma forma mais bonitinha, pois o site não aceita
+    }
