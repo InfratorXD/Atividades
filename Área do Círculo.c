33d626c2fbce5ad6097d917e1511ffc603507455
InfratorXD <guiguicazeee@gmail.com>
2023-08-29 09:45:23 -0300

    circulo

---
+++ Área do Círculo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+double raio;
+double n = 3.14159;
+double area;
+
+scanf("%lf", &raio);
+
+area = n * pow(raio, 2);
+
+printf("A=%.4lf\n", area);
+
+
+ return 0;
+}
